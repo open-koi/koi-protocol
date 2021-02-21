5dddde6ac5b3d175b3c9f1442b6ab0eb09bad5ed
Sebhatu <abuga8@yahoo.com>
2021-02-20 17:49:47 -0800

    registering gateway and  state obj  have been updated

--- src/Gateway.js
+++ src/Gateway.js
@@ -3,26 +3,35 @@ export function Gateway(state, action) {
     const input = action.input;
     const caller = action.caller;
     const balances = state.balances;
+    const url = input.url;
+    const publicKey = input.publicKey;
 
-    const gateway = input.gateway;
 
-    if (!gateway) {
+    if (!url) {
         throw new ContractError('No gateway specified');
     }
 
-    if (!gateway.match(/^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/gi)) {
+    if (!publicKey) {
+        throw new ContractError('No publicKey specified');
+    }
+
+    if (!url.match(/^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/gi)) {
         throw new ContractError('The gateway must be a valid URL or IP');
     }
-    if(balances[caller] < 1){
+    if (balances[caller] < 1) {
 
-        throw new ContractError('you need min 1 KOI to register data');
+        throw new ContractError('you need min 1 KOI to register gateway');
 
     }
-    
-    
-    // burn 1 koi per registeration 
+
+
+    // burn 1 koi per registration 
     balances[caller] -= 1;
-    gateways[caller] = gateway;
+    gateways[caller] = {
+        "url": url,
+        "publicKey": publicKey,
+        "rate": 0
+    }
 
     return { state }
 }
---
+++ src/SubmitTrafficlog.js
@@ -0,0 +1,13 @@
+export async function SubmitTrafficLog(state, action) {
+    const input = action.input,
+    const batchTxId = input.batchTxId,
+    const gateWayId = input.gateWayId,
+
+
+
+
+
+    return { state }
+
+
+}
